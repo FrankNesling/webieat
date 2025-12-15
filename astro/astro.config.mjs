5615d2cb627d6910f5559068b81ec017b0b98bb4
franknesling <matthias.gnilsen@hotmail.com>
2025-12-16 00:15:18 +0100

    try assets prefix

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://webie.at',
-
+  build: {
+    assetsPrefix: './astro',
+  },
   vite: {
     plugins: [tailwindcss()]
   }
