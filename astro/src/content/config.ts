8baa2d6e64356b2057af0608bca9c91dc9fd784b
franknesling <matthias.gnilsen@hotmail.com>
2025-12-18 02:08:54 +0100

    content data structuring

---
+++ astro/src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+import { file } from 'astro/loaders';
+
+const skills = defineCollection({
+  loader: file("src/data/skills.json"),
+  schema: z.array(
+    z.object({
+      id: z.number(),
+      color: z.string(),
+      headline: z.string(),
+      description: z.string(),
+    })
+  ),
+});
+
+export const collections = { skills };
