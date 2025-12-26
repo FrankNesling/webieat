3a6a044794aa5a4b278e1de9a1c6754930e9a8a6
franknesling <matthias.gnilsen@hotmail.com>
2025-12-26 23:37:18 +0100

    add skill type

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -5,9 +5,19 @@ const skills = defineCollection({
   loader: file("src/data/skills.json"),
   schema: z.object({
       id: z.number(),
+      isProject: z.boolean(),
+      isApplication: z.boolean(),
+      isTask: z.boolean(),
       headline: z.string(),
       description: z.string(),
     }),
 });
+
+
+
+// Skill -> Project, Application (Mobile App or Program), Task (Script or Task), Concept
+// Tags -> Visual Computing, API, Database, Web, Mobile App, Machine Learning/AI, Authentication
+// Tools -> MS Entra, Clerk, D365, CMSes?, Wordpress, VS, VS Code, XCode, Eclipse, Postman, Insomnia
+
 
 export const collections = { skills };
