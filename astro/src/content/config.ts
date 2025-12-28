4a9a86fc6495a14e116088b22b400a903ae09ffe
franknesling <matthias.gnilsen@hotmail.com>
2025-12-28 01:21:30 +0100

    extend skill list

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -15,17 +15,18 @@ const skills = defineCollection({
       tools: z.array(z.string()),
       topic: z.string(),
       description: z.string(),
+      url: z.string()
     }),
 });
 
 
 
 // Skill -> Project, Application (Mobile App or Program), Task (Script or Task), Concept
-// Tags -> Visual Computing, API, Database, Desktop Program, Web, Mobile App, Machine Learning/AI, Network, Computer Graphics, DevOps, Data science, Compiler, Algorithms
+// Tags -> Visual Computing, API, Database, Desktop Program, Game, Web, Mobile App, Cloud Application, Machine Learning/AI, Network, Computer Graphics, Parallel Programming, DevOps, Data science, Compiler, Algorithms
 // Tools -> MS Entra, Clerk, D365, CMSes?, Wordpress, VS, VS Code, XCode, Eclipse, Postman, Insomnia
 
 // Tool "types" will be specified in the filter list of the index page, no other need
-// Tool types -> IDE, Authentication, Networking, Versioning, Hosting, Database, CRM, CMS, Project management, Automation, Webdevelopment
+// Tool types -> IDE, Authentication, Networking, Versioning, Hosting, Database, CRM, CMS, Project management, Automation, Webdevelopment, Containerazation
 
 // somewhere I need to specify I am familiar with all Windows, MacOS and Linxu (Ubuntu, Fedora)
 
