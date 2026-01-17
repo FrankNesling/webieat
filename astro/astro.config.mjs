9da818c9a08051ebb60a2592c4103a62b94bc581
franknesling <matthias.gnilsen@hotmail.com>
2026-01-17 16:35:39 +0100

    asset prefix v3

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   site: 'https://franknesling.github.io/webieat',
   base: '',
   build: {
-    assetsPrefix: './',
+    assetsPrefix: './webieat',
   },
   vite: {
     plugins: [tailwindcss()]
