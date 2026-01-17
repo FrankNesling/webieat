10de5ab8fcf193b86c5a92ee038daf826fed35e7
franknesling <matthias.gnilsen@hotmail.com>
2026-01-17 16:34:05 +0100

    retry url and navigation

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://franknesling.github.io',
-  base: '/webieat',
+  site: 'https://franknesling.github.io/webieat',
+  base: '',
   build: {
     assetsPrefix: './',
   },
